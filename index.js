967b6ea1b26189300ef0dfae90e35d748600c9af
Anthony <anthony.onyango@student.moringaschool.com>
2024-06-29 01:48:14 +0300

    update objects

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Anthony'
+};
+//updateEmployeeWithKeyAndValue
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const updatedEmployee = {...employee};
+    updatedEmployee[key] = value;
+    return updatedEmployee;
+}
+//destructivelyUpdateEmployeeWithKeyAndValue
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    // Update  employee object with the new key-value  (destructive update)
+    employee[key] = value;
+
+    // Return the  updated employee object
+    return employee;
+}
+
+//deleteFromEmployeeByKey
+function deleteFromEmployeeByKey(employee, key) {
+    // Create copy of the employee object 
+    const updatedEmployee = { ...employee };
+
+    if (key in updatedEmployee) {
+        // Use delete to remove the property with the specified-key
+        delete updatedEmployee[key];
+    }
+
+    // Return the updated employee object 
+    return updatedEmployee;
+}
+
+//destructivelyDeleteFromEmployeeByKey
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
